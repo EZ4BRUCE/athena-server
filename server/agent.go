48bbc2b75662d8bd074d4166075d553552a594fa
EZ4BRUCE <lhy122786302@gmail.com>
2022-02-14 14:27:23 +0800

    修改注释

--- server/agent.go
+++ server/agent.go
@@ -4,19 +4,16 @@ import (
 	pb "github.com/EZ4BRUCE/athena-proto/proto"
 )
 
-// 全局定义每个agent主机的结构体
+// 根据用户注册提供的参数定义每个agent主机的结构体
 type Agent struct {
-	UId              string
-	CheckAliveTime   int32
-	AggregationTime  int32
-	CheckAliveStatus bool
-	IsDead           bool
-	// 记录主机的最近上报信息（用于聚合数据）
-	// key:metric value:存储对应metric的最近收到的report的channel
-	MetricMap map[string]chan *pb.ReportReq
+	UId              string                        // 用户注册后发放的标识UId
+	CheckAliveTime   int32                         // 连接诊断时间
+	AggregationTime  int32                         // 聚合时间(/单位聚合粒度)
+	CheckAliveStatus bool                          // 当前连接存活状态
+	IsDead           bool                          // 标识该Agent是否已断开
+	MetricMap        map[string]chan *pb.ReportReq // 最近上报信息(用于聚合数据)
 }
 
 var (
-	// RegisterMap记录每个已注册的主机
-	RegisterMap map[string]*Agent
+	RegisterMap map[string]*Agent // RegisterMap记录每个已注册的主机
 )
